9fed657e5db3369054178d0669c74f171775836f
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-09-25 12:56:18 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan15-Logika/src/if1/pkg10118038/latihan15/logika/IF110118038Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan15.logika;
+
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : program ini berisi program yang akan menampilkan Logika
+ */
+public class IF110118038Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a  = " + !a);
+        System.out.println("!(a && b) = " + ! (a && b));
+    }
+    
+}
